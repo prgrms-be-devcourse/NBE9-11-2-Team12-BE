e6612c773575cb61357a638aef7bc3cc7ac709b6
MMuhwon <mwon1590@gmail.com>
2026-04-14 16:59:55 +0900

    feat: 회원가입/로그인 테스트 코드 구현

--- backend/src/test/java/com/rungo/api/domain/auth/controller/AuthControllerTest.java
+++ backend/src/test/java/com/rungo/api/domain/auth/controller/AuthControllerTest.java
@@ -7,6 +7,7 @@ import com.rungo.api.domain.users.enumtype.Gender;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(AuthController.class)
 class AuthControllerTest {
 
