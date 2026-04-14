f695d9396b56ba6a45cd1a63f9af083824372ad3
Kim Dongho <kdongho0406@gmail.com>
2026-04-14 09:51:53 +0900

    feat: 접수 생성 & 취소 DTO 초기 작성 (#14)
    * feat: 접수 생성 & 취소 DTO 초기 작성
    
    * feat: 접수 취소 응답 DTO 필드 수정
    
    * refactor: 접수 도메인 application을 registration으로 변경

--- backend/src/main/java/com/rungo/api/domain/registration/entity/Registration.java
+++ backend/src/main/java/com/rungo/api/domain/registration/entity/Registration.java
@@ -1,8 +1,8 @@
-package com.rungo.api.domain.application.entity;
+package com.rungo.api.domain.registration.entity;
 
-import com.rungo.api.domain.application.enumtype.ApplicationStatus;
 import com.rungo.api.domain.marathon.course.entity.Course;
 import com.rungo.api.domain.marathon.marathon.entity.Marathon;
+import com.rungo.api.domain.registration.enumtype.RegistrationStatus;
 import com.rungo.api.domain.users.entity.Users;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -15,48 +15,36 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(
-        name = "applications",
+        name = "registrations",
         uniqueConstraints = {
-                @UniqueConstraint(name = "uk_application_user_marathon", columnNames = {"user_id", "marathon_id"})
+                @UniqueConstraint(name = "uk_registration_user_marathon", columnNames = {"user_id", "marathon_id"})
         }
 )
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class Application {
+public class Registration {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /**
-     * 신청한 사용자
-     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private Users user;
 
-    /**
-     * 신청한 코스
-     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id", nullable = false)
     private Course course;
 
-    /**
-     * 중복 신청 방지용/조회 편의용
-     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "marathon_id", nullable = false)
     private Marathon marathon;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
-    private ApplicationStatus status;
+    private RegistrationStatus status;
 
-    /**
-     * 신청 당시 스냅샷
-     */
     @Column(name = "snap_name", nullable = false, length = 50)
     private String snapName;
 
@@ -79,13 +67,9 @@ public class Application {
     @CreatedDate
     private LocalDateTime appliedAt;
 
-    /**
-     *  취소 시간은 직접 세팅
-     */
     @Column(name = "canceled_at")
     private LocalDateTime canceledAt;
 
     @Column(name = "agreed_terms", nullable = false)
     private boolean agreedTerms;
-
 }
