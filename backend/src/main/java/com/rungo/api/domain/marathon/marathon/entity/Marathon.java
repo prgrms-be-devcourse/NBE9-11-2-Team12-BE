3d50700d04e29a1face407b03605fc8b985a3340
ksg1103 <ksgwkd1103@naver.com>
2026-04-14 11:39:03 +0900

    feat:Entity 생성자 추가, 서비스 로직,예외 처리

--- backend/src/main/java/com/rungo/api/domain/marathon/marathon/entity/Marathon.java
+++ backend/src/main/java/com/rungo/api/domain/marathon/marathon/entity/Marathon.java
@@ -55,6 +55,25 @@ public class Marathon {
     @OneToMany(mappedBy = "marathon", cascade =  CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     private List<Course> courses = new ArrayList<>();
 
+    public Marathon(
+            Users organizer,
+            String title,
+            String region,
+            LocalDate eventDate,
+            String posterImageUrl,
+            LocalDateTime registrationStartAt,
+            LocalDateTime registrationEndAt,
+            MarathonStatus status
+    ) {
+        this.organizer = organizer;
+        this.title = title;
+        this.region = region;
+        this.eventDate = eventDate;
+        this.posterImageUrl = posterImageUrl;
+        this.registrationStartAt = registrationStartAt;
+        this.registrationEndAt = registrationEndAt;
+        this.status = status;
+    }
     public void addCourse(Course course){
         this.courses.add(course);
         course.setMarathon(this);
