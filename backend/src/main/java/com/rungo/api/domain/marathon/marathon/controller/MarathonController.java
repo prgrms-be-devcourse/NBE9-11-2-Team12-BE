f8c4f6ee1c0d04cb8cb7f6e1a431e5608b7f89d3
ksg1103 <ksgwkd1103@naver.com>
2026-04-15 11:48:50 +0900

    feat: MarathonController 구현

---
+++ backend/src/main/java/com/rungo/api/domain/marathon/marathon/controller/MarathonController.java
@@ -0,0 +1,37 @@
+package com.rungo.api.domain.marathon.marathon.controller;
+
+import com.rungo.api.domain.marathon.marathon.dto.CreateMarathonReq;
+import com.rungo.api.domain.marathon.marathon.dto.CreateMarathonRes;
+import com.rungo.api.domain.marathon.marathon.service.MarathonService;
+import com.rungo.api.global.exception.CustomException;
+import com.rungo.api.global.exception.ErrorCode;
+import com.rungo.api.global.response.ApiResponse;
+import com.rungo.api.global.security.SecurityUser;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/marathons")
+@RequiredArgsConstructor
+public class MarathonController {
+
+    private final MarathonService marathonService;
+
+    @PostMapping
+    public ApiResponse<CreateMarathonRes> createMarathon(
+            @AuthenticationPrincipal SecurityUser user,
+            @Valid @RequestBody CreateMarathonReq req
+    ) {
+        if (user == null) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        CreateMarathonRes res = marathonService.createMarathon(user.getId(), req);
+        return ApiResponse.ok(res);
+    }
+}
