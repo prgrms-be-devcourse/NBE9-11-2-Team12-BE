1474d140ac0ecc75059fabb98cf23733ddc5724f
ksg1103 <ksgwkd1103@naver.com>
2026-04-16 10:48:11 +0900

    feat:마라톤 주최자측 취소용 DTO생성

---
+++ backend/src/main/java/com/rungo/api/domain/marathon/marathon/dto/delete/CancelMarathonRes.java
@@ -0,0 +1,31 @@
+package com.rungo.api.domain.marathon.marathon.dto.delete;
+
+
+import com.rungo.api.domain.marathon.course.entity.Course;
+import com.rungo.api.domain.marathon.marathon.dto.CourseItemRes;
+import com.rungo.api.domain.marathon.marathon.entity.Marathon;
+import com.rungo.api.domain.marathon.marathon.enumtype.MarathonStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record CancelMarathonRes(
+        Long marathonId,
+        String title,
+        LocalDate eventDate,
+        MarathonStatus status,
+        List<CancelCourseItemRes> courses
+) {
+    public static CancelMarathonRes from(Marathon marathon) {
+        return new CancelMarathonRes(
+                marathon.getId(),
+                marathon.getTitle(),
+                marathon.getEventDate(),
+                marathon.getStatus(),
+                marathon.getCourses().stream()
+                        .map(CancelCourseItemRes::from)
+                        .toList()
+
+        );
+    }
+}
