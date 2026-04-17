d89ea4bf6c4db2d893305c1d76ab4482b17f5a86
ksg1103 <ksgwkd1103@naver.com>
2026-04-17 16:18:15 +0900

    chore: 쿼리문 EntityGraph로 변경

--- backend/src/main/java/com/rungo/api/domain/marathon/marathon/repository/MarathonRepository.java
+++ backend/src/main/java/com/rungo/api/domain/marathon/marathon/repository/MarathonRepository.java
@@ -4,6 +4,7 @@ import com.rungo.api.domain.marathon.marathon.entity.Marathon;
 import com.rungo.api.domain.marathon.marathon.enumtype.MarathonStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,12 +13,7 @@ import java.util.Optional;
 
 public interface MarathonRepository extends JpaRepository<Marathon, Long> {
     Page<Marathon> findByStatusIn(List<MarathonStatus> statuses, Pageable pageable);
-    @Query("""
-    SELECT DISTINCT m
-    FROM Marathon m
-    LEFT JOIN FETCH m.courses
-    WHERE m.id = :marathonId
-      AND m.organizer.id = :organizerId
-""")
+
+    @EntityGraph(attributePaths = {"courses"})
     Optional<Marathon> findByIdAndOrganizerIdWithCourses(Long marathonId, Long organizerId);
 }
