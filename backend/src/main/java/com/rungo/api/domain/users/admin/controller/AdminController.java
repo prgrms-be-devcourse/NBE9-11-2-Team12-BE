bdfa9c9f549be6077f45d7b1b7bfd6d6258a08b3
ksg1103 <ksgwkd1103@naver.com>
2026-04-20 15:21:52 +0900

    Feat: admin approve orginzer(#118 )
    * feat: 주최자 권한 부여 서비스 로직 작성
    
    * feat:주최자 권한부여 controller 구현
    
    * chore: 주최자 권한부여 테스트 완료
    
    * chore:응답 수정
    
    * chore: 전용 DTO 수정

---
+++ backend/src/main/java/com/rungo/api/domain/users/admin/controller/AdminController.java
@@ -0,0 +1,42 @@
+package com.rungo.api.domain.users.admin.controller;
+
+import com.rungo.api.domain.users.admin.dto.AdminApproveRes;
+import com.rungo.api.domain.users.admin.service.AdminService;
+import com.rungo.api.domain.users.dto.MyProfileRes;
+import com.rungo.api.domain.users.entity.Users;
+import com.rungo.api.global.response.ApiResponse;
+import com.rungo.api.global.security.SecurityUser;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @PatchMapping("/{userId}/organizer")
+
+    public ResponseEntity<ApiResponse<AdminApproveRes>> approveOrganizer(
+
+            @AuthenticationPrincipal SecurityUser admin,
+
+            @PathVariable @NotNull Long userId
+
+    ) {
+
+        AdminApproveRes adminApproveRes =adminService.approveOrganizer(admin.getId(), userId);
+
+        return ResponseEntity.ok(ApiResponse.ok(adminApproveRes));
+
+    }
+}
