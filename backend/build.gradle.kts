ae9de3b81dbdc6f1b0e8ef24fd1ff09c3a7c0232
최윤서 <128898996+YunseoChoe@users.noreply.github.com>
2026-04-21 12:47:46 +0900

    feat: 토큰 재발급 동시성 구현 및 테스트 코드 (#119)
    * feat: 토큰 재발급 동시성 구현 및 테스트 코드 (#119)
    
    * feat: lock 미획득 시 클라이언트 재시도 정책 (#119)
    
    * chore: lock 관련 시간 변수 yaml 파일에 추가 (#119)
    
    * fix: 락 관련 시간 변경 (#119)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -38,7 +38,9 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
 
 	implementation("me.paulschwarz:spring-dotenv:4.0.0")
+
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.redisson:redisson-spring-boot-starter:3.27.2")
 
 	// 이메일 발송 의존성 추가
 	implementation("org.springframework.boot:spring-boot-starter-mail")
